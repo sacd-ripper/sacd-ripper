40a9eccdb872856a17b8e3226a06e702fdd1dd7c
mr_wicked@hotmail.com <mr_wicked@hotmail.com@e4565217-ec48-2de7-c894-ee36b58b2fd4>
2011-01-07 11:16:59 +0000

    - added channel parsing & printing

--- trunk/sacdread/scarletbook_read.c
+++ trunk/sacdread/scarletbook_read.c
@@ -57,6 +57,7 @@ static const uint8_t my_friendly_zeros[2048];
 
 /* Prototypes for internal functions */
 static int scarletbook_read_master_toc(scarletbook_handle_t *);
+static int scarletbook_read_channel_toc(scarletbook_handle_t *, int);
 
 scarletbook_handle_t *scarletbook_open(sacd_reader_t *sacd, int title) {
   scarletbook_handle_t *sb;
@@ -73,6 +74,39 @@ scarletbook_handle_t *scarletbook_open(sacd_reader_t *sacd, int title) {
     scarletbook_close(sb);
   }
 
+  if (sb->master_toc->channel_1_toc_area_1_start) {
+	  sb->channel_data[sb->channel_count] = malloc(sb->master_toc->channel_1_toc_area_size * SACD_LSN_SIZE);
+	  if(!sb->channel_data[sb->channel_count]) {
+		  scarletbook_close(sb);
+		  return 0;
+	  }
+
+	  if (!sacd_read_block_raw(sacd, sb->master_toc->channel_1_toc_area_1_start, sb->master_toc->channel_1_toc_area_size, sb->channel_data[sb->channel_count]))
+		  return 0;
+
+	  if (!scarletbook_read_channel_toc(sb, sb->channel_count)) {
+		  fprintf(stderr, "libsacdread: Can't read Channel TOC 1.\n");
+	  } 
+	  else 
+		++sb->channel_count;
+  }
+  if (sb->master_toc->channel_2_toc_area_1_start) {
+	  sb->channel_data[sb->channel_count] = malloc(sb->master_toc->channel_2_toc_area_size * SACD_LSN_SIZE);
+	  if(!sb->channel_data[sb->channel_count]) {
+		  scarletbook_close(sb);
+		  return 0;
+	  }
+
+	  if (!sacd_read_block_raw(sacd, sb->master_toc->channel_2_toc_area_1_start, sb->master_toc->channel_2_toc_area_size, sb->channel_data[sb->channel_count]))
+		  return 0;
+
+	  if (!scarletbook_read_channel_toc(sb, sb->channel_count)) {
+		  fprintf(stderr, "libsacdread: Can't read Channel TOC 2.\n");
+	  } else
+		++sb->channel_count;
+  }
+
+
   return sb;
 }
 
@@ -82,6 +116,12 @@ void scarletbook_close(scarletbook_handle_t *handle) {
 
   if(handle->master_data)
     free(handle->master_data);
+
+  if(handle->channel_data[0])
+	  free(handle->channel_data[0]);
+
+  if(handle->channel_data[1])
+	  free(handle->channel_data[1]);
 
   memset(handle, 0, sizeof(scarletbook_handle_t));
 
@@ -113,9 +153,9 @@ static int scarletbook_read_master_toc(scarletbook_handle_t *handle) {
   ntoh16(master_toc->album_sequence_number);
   ntoh32(master_toc->channel_1_toc_area_1_start);
   ntoh32(master_toc->channel_1_toc_area_2_start);
-  ntoh32(master_toc->channel_1_toc_area_size);
+  ntoh16(master_toc->channel_1_toc_area_size);
   ntoh32(master_toc->channel_2_toc_area_1_start);
-  ntoh16(master_toc->channel_2_toc_area_2_start);
+  ntoh32(master_toc->channel_2_toc_area_2_start);
   ntoh16(master_toc->channel_2_toc_area_size);
   ntoh16(master_toc->disc_date_year);
 
@@ -182,4 +222,77 @@ static int scarletbook_read_master_toc(scarletbook_handle_t *handle) {
   }
 
   return 1;
+}
+
+static int scarletbook_read_channel_toc(scarletbook_handle_t *handle, int channel_nr) {
+	int i, text_channel_counter;
+	channel_toc_t *channel_toc;
+    uint8_t *channel_data;
+	uint8_t *p;
+
+	p = channel_data = handle->channel_data[channel_nr];
+	channel_toc = handle->channel_toc[channel_nr] = (channel_toc_t*) channel_data;
+
+	if (strncmp("TWOCHTOC", channel_toc->id, 8) != 0 && strncmp("MULCHTOC", channel_toc->id, 8) != 0) {
+		fprintf(stderr, "libsacdread: Not a valid Channel TOC!\n");
+		return 0;
+	}
+
+	ntoh16(channel_toc->version);
+	ntoh16(channel_toc->size);
+	ntoh16(channel_toc->track_count);
+	ntoh32(channel_toc->track_position);
+	ntoh32(channel_toc->track_length);
+	ntoh16(channel_toc->area_description_offset);
+	ntoh16(channel_toc->copyright_offset);
+	ntoh16(channel_toc->area_description_phonetic_offset);
+	ntoh16(channel_toc->copyright_phonetic_offset);
+
+	if (channel_toc->version != SUPPORT_SCARLETBOOK_VERSION) {
+		fprintf(stderr, "libsacdread: Unsupported version: %2i.%2i\n", (channel_toc->version >> 8) & 0xff, channel_toc->version & 0xff);
+		return 0;
+	}
+
+	// Channel TOC size is SACD_LSN_SIZE
+	p += SACD_LSN_SIZE;
+
+	text_channel_counter = 0;
+	while (p < (channel_data + channel_toc->size * SACD_LSN_SIZE)) {
+		if (strncmp(p, "SACDTTxt", 8) == 0) {
+			channel_text_t *channel_text;
+			channel_text = handle->channel_text[channel_nr][text_channel_counter] = (channel_text_t*) p;
+			for (i = 0; i < channel_toc->track_count; i++) {
+				ntoh16(channel_text->track_text_position[i]);
+			}
+			++text_channel_counter;
+			p += SACD_LSN_SIZE;
+		} else if (strncmp(p, "SACD_IGL", 8) == 0) {
+			handle->channel_isrc[channel_nr] = (channel_isrc_t*) p;
+			p += SACD_LSN_SIZE * 2;
+		} else if (strncmp(p, "SACD_ACC", 8) == 0) {
+			// skip
+			p += SACD_LSN_SIZE * 32;
+		} else if (strncmp(p, "SACDTRL1", 8) == 0) {
+			channel_tracklist_offset_t *tracklist;
+			tracklist = handle->channel_tracklist_offset[channel_nr] = (channel_tracklist_offset_t*) p;
+			for (i = 0; i < channel_toc->track_count; i++) {
+				ntoh32(tracklist->track_start_lsn[i]);
+				ntoh32(tracklist->track_stop_lsn[i]);
+			}
+			p += SACD_LSN_SIZE;
+		} else if (strncmp(p, "SACDTRL2", 8) == 0) {
+			handle->channel_tracklist_time[channel_nr] = (channel_tracklist_time_t*) p;
+			p += SACD_LSN_SIZE;
+		} else {
+			break;
+		}
+	}
+
+	for (i = 0; i < channel_toc->text_channel_count; i++) {
+		//channel_toc->languages
+	}
+
+	// tracks
+
+	return 1;
 }
