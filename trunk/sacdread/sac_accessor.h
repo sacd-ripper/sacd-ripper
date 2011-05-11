d7343af49c617a3b1e53a84f810dc1146dbd8b48
mr_wicked@hotmail.com <mr_wicked@hotmail.com@e4565217-ec48-2de7-c894-ee36b58b2fd4>
2011-05-11 14:56:46 +0000

    changed indentation

--- trunk/sacdread/sac_accessor.h
+++ trunk/sacdread/sac_accessor.h
@@ -20,7 +20,7 @@ CDECL_BEGIN
 #define INTR_STOP_SHIFT     1
 #define INTR_HALT_SHIFT     2
 #define INTR_DMA_SHIFT      3
-#define INTR_SPU_MB_SHIFT   4 
+#define INTR_SPU_MB_SHIFT   4
 #define INTR_PPU_MB_MASK    (0x1 << INTR_PPU_MB_SHIFT)
 #define INTR_STOP_MASK      (0x1 << INTR_STOP_SHIFT)
 #define INTR_HALT_MASK      (0x1 << INTR_HALT_SHIFT)
@@ -29,10 +29,10 @@ CDECL_BEGIN
 
 #define SPU_INTR_CLASS_2      ( 2 )
 
-#define MAX_PHYSICAL_SPU       6 
+#define MAX_PHYSICAL_SPU       6
 #define MAX_RAW_SPU            1
 
-#define EIEIO __asm__ volatile("eieio")
+#define EIEIO __asm__ volatile ("eieio")
 
 /** configuration of sac accessor thread */
 #define PRIMARY_PPU_THREAD_PRIO ( 1001 )
@@ -42,91 +42,91 @@ CDECL_BEGIN
 #define EXIT_SAC_CMD ( 0xFEFEFEFF )
 
 #ifdef USE_ISOSELF
-	
+
 	#define SAC_MODULE_LOCATION "/dev_flash/vsh/module/SacModule.spu.isoself"
-	
-	static inline int sys_isoself_spu_create(sys_raw_spu_t *id, uint8_t *source_spe) {
-	    system_call_6(230, (uint32_t) id, (uint32_t) source_spe, 0, 0, 0, 0);
-	    return_to_user_prog(int);
-	}
-	
-	static inline int sys_isoself_spu_destroy(sys_raw_spu_t id) {
-	    system_call_1(231, id);
-	    return_to_user_prog(int);
-	}
-	
-	static inline int sys_isoself_spu_start(sys_raw_spu_t id) {
-	    system_call_1(232, id);
-	    return_to_user_prog(int);
-	}
-	
-	static inline int sys_isoself_spu_create_interrupt_tag(sys_raw_spu_t id,
-	        sys_class_id_t class_id,
-	        sys_hw_thread_t hwthread,
-	        sys_interrupt_tag_t *intrtag) {
-	    system_call_4(233, id, class_id, hwthread, (uint32_t) intrtag);
-	    return_to_user_prog(int);
-	}
-	
-	static inline int sys_isoself_spu_set_int_mask(sys_raw_spu_t id,
-	        sys_class_id_t class_id,
-	        __CSTD uint64_t mask) {
-	    system_call_3(234, id, class_id, mask);
-	    return_to_user_prog(int);
-	}
-	
-	static inline int sys_isoself_spu_set_int_stat(sys_raw_spu_t id,
-	        sys_class_id_t class_id,
-	        __CSTD uint64_t stat) {
-	    system_call_3(236, id, class_id, stat);
-	    return_to_user_prog(int);
-	}
-	
-	static inline int sys_isoself_spu_get_int_stat(sys_raw_spu_t id,
-	        sys_class_id_t class_id,
-	        __CSTD uint64_t * stat) {
-	    system_call_3(237, id, class_id, (uint32_t) stat);
-	    return_to_user_prog(int);
-	}
-	
-	static inline int sys_isoself_spu_read_puint_mb(sys_raw_spu_t id,
-	        __CSTD uint32_t * value) {
-	    system_call_2(240, id, (uint32_t) value);
-	    return_to_user_prog(int);
-	}	
-	
+
+static inline int sys_isoself_spu_create(sys_raw_spu_t *id, uint8_t *source_spe) {
+	system_call_6(230, (uint32_t) id, (uint32_t) source_spe, 0, 0, 0, 0);
+	return_to_user_prog(int);
+}
+
+static inline int sys_isoself_spu_destroy(sys_raw_spu_t id) {
+	system_call_1(231, id);
+	return_to_user_prog(int);
+}
+
+static inline int sys_isoself_spu_start(sys_raw_spu_t id) {
+	system_call_1(232, id);
+	return_to_user_prog(int);
+}
+
+static inline int sys_isoself_spu_create_interrupt_tag(sys_raw_spu_t id,
+                                                       sys_class_id_t class_id,
+                                                       sys_hw_thread_t hwthread,
+                                                       sys_interrupt_tag_t *intrtag) {
+	system_call_4(233, id, class_id, hwthread, (uint32_t) intrtag);
+	return_to_user_prog(int);
+}
+
+static inline int sys_isoself_spu_set_int_mask(sys_raw_spu_t id,
+                                               sys_class_id_t class_id,
+                                               __CSTD uint64_t mask) {
+	system_call_3(234, id, class_id, mask);
+	return_to_user_prog(int);
+}
+
+static inline int sys_isoself_spu_set_int_stat(sys_raw_spu_t id,
+                                               sys_class_id_t class_id,
+                                               __CSTD uint64_t stat) {
+	system_call_3(236, id, class_id, stat);
+	return_to_user_prog(int);
+}
+
+static inline int sys_isoself_spu_get_int_stat(sys_raw_spu_t id,
+                                               sys_class_id_t class_id,
+                                               __CSTD uint64_t * stat) {
+	system_call_3(237, id, class_id, (uint32_t) stat);
+	return_to_user_prog(int);
+}
+
+static inline int sys_isoself_spu_read_puint_mb(sys_raw_spu_t id,
+                                                __CSTD uint32_t * value) {
+	system_call_2(240, id, (uint32_t) value);
+	return_to_user_prog(int);
+}
+
 #else
 	#define SAC_MODULE_LOCATION "/dev_usb000/SacModule.spu.elf"
 #endif
 
 enum {
-      SAC_CMD_INITIALIZE     = 0
-    , SAC_CMD_EXIT           = 1
-    , SAC_CMD_GENERATE_KEY_1 = 2
-    , SAC_CMD_VALIDATE_KEY_1 = 3
-    , SAC_CMD_GENERATE_KEY_2 = 4
-    , SAC_CMD_VALIDATE_KEY_2 = 5
-    , SAC_CMD_VALIDATE_KEY_3 = 6
-    , SAC_CMD_ENCRYPT = 7
-    , SAC_CMD_DECRYPT = 8
+	SAC_CMD_INITIALIZE     = 0
+	, SAC_CMD_EXIT           = 1
+	, SAC_CMD_GENERATE_KEY_1 = 2
+	, SAC_CMD_VALIDATE_KEY_1 = 3
+	, SAC_CMD_GENERATE_KEY_2 = 4
+	, SAC_CMD_VALIDATE_KEY_2 = 5
+	, SAC_CMD_VALIDATE_KEY_3 = 6
+	, SAC_CMD_ENCRYPT = 7
+	, SAC_CMD_DECRYPT = 8
 } sac_command_t;
 
 typedef struct {
-    uint8_t *buffer;
+	uint8_t *buffer;
 
-    uint8_t *module_buffer;
-    unsigned int module_size;
+	uint8_t *module_buffer;
+	unsigned int module_size;
 
-    sys_ppu_thread_t handler;
+	sys_ppu_thread_t handler;
 
-    sys_cond_t mmio_cond;
-    sys_mutex_t mmio_mutex;
+	sys_cond_t mmio_cond;
+	sys_mutex_t mmio_mutex;
 
-    sys_raw_spu_t id;
-    sys_interrupt_thread_handle_t ih;
-    sys_interrupt_tag_t intrtag;
+	sys_raw_spu_t id;
+	sys_interrupt_thread_handle_t ih;
+	sys_interrupt_tag_t intrtag;
 
-    uint32_t error_code;
+	uint32_t error_code;
 } sac_accessor_t;
 
 extern sac_accessor_t *sa;
--- trunk/sacdread/sacd_lv2_storage.h
+++ trunk/sacdread/sacd_lv2_storage.h
@@ -18,54 +18,54 @@
 CDECL_BEGIN
 
 /*********************************************************************
- * Generic Packet commands, MMC commands, and such
- *********************************************************************/
+* Generic Packet commands, MMC commands, and such
+*********************************************************************/
 
 /* The generic packet command opcodes for CD/DVD Logical Units,
  * From Table 57 of the SFF8090 Ver. 3 (Mt. Fuji) draft standard. */
-#define GPCMD_GET_CONFIGURATION		    0x46
+#define GPCMD_GET_CONFIGURATION             0x46
 #define GPCMD_GET_EVENT_STATUS_NOTIFICATION 0x4a
-#define GPCMD_MODE_SELECT_10		    0x55
-#define GPCMD_MODE_SENSE_10		    0x5a
-#define GPCMD_READ_CD			    0xbe
-#define GPCMD_READ_DVD_STRUCTURE	    0xad
-#define GPCMD_READ_TRACK_RZONE_INFO	    0x52
-#define GPCMD_READ_TOC_PMA_ATIP		    0x43
-#define GPCMD_REPORT_KEY		    0xa4
-#define GPCMD_SEND_KEY			    0xa3
+#define GPCMD_MODE_SELECT_10                0x55
+#define GPCMD_MODE_SENSE_10                 0x5a
+#define GPCMD_READ_CD                       0xbe
+#define GPCMD_READ_DVD_STRUCTURE            0xad
+#define GPCMD_READ_TRACK_RZONE_INFO         0x52
+#define GPCMD_READ_TOC_PMA_ATIP             0x43
+#define GPCMD_REPORT_KEY                    0xa4
+#define GPCMD_SEND_KEY                      0xa3
 
-#define LV2_STORAGE_SEND_ATAPI_COMMAND	(1)
+#define LV2_STORAGE_SEND_ATAPI_COMMAND  (1)
 
 struct lv2_atapi_cmnd_block {
-    uint8_t pkt[32]; /* packet command block           */
-    uint32_t pktlen; /* should be 12 for ATAPI 8020    */
-    uint32_t blocks;
-    uint32_t block_size;
-    uint32_t proto; /* transfer mode                  */
-    uint32_t in_out; /* transfer direction             */
-    uint32_t unknown;
+	uint8_t pkt[32]; /* packet command block           */
+	uint32_t pktlen; /* should be 12 for ATAPI 8020    */
+	uint32_t blocks;
+	uint32_t block_size;
+	uint32_t proto; /* transfer mode                  */
+	uint32_t in_out; /* transfer direction             */
+	uint32_t unknown;
 } __attribute__((packed));
 
 typedef struct {
-    int fd;
-    uint64_t device_id;
-    sys_ppu_thread_t thread_id;
-    sys_mutex_t read_async_mutex_id;
-    sys_event_queue_t queue;
+	int fd;
+	uint64_t device_id;
+	sys_ppu_thread_t thread_id;
+	sys_mutex_t read_async_mutex_id;
+	sys_event_queue_t queue;
 	int io_buffer;
 	int io_buffer_piece;
 } sac_accessor_driver_t;
 
 enum lv2_atapi_proto {
-    NON_DATA_PROTO = 0,
-    PIO_DATA_IN_PROTO = 1,
-    PIO_DATA_OUT_PROTO = 2,
-    DMA_PROTO = 3
+	NON_DATA_PROTO = 0,
+	PIO_DATA_IN_PROTO = 1,
+	PIO_DATA_OUT_PROTO = 2,
+	DMA_PROTO = 3
 };
 
 enum lv2_atapi_in_out {
-    DIR_WRITE = 0, /* memory -> device */
-    DIR_READ = 1 /* device -> memory */
+	DIR_WRITE = 0, /* memory -> device */
+	DIR_READ = 1 /* device -> memory */
 };
 
 static inline void init_atapi_cmnd_block(
@@ -73,61 +73,61 @@ static inline void init_atapi_cmnd_block(
         , uint32_t block_size
         , uint32_t proto
         , uint32_t type) {
-    memset(atapi_cmnd, 0, sizeof(struct lv2_atapi_cmnd_block));
-    atapi_cmnd->pktlen = 12;
-    atapi_cmnd->blocks = 1;
-    atapi_cmnd->block_size = block_size; /* transfer size is block_size * blocks */
-    atapi_cmnd->proto = proto;
-    atapi_cmnd->in_out = type;
+	memset(atapi_cmnd, 0, sizeof(struct lv2_atapi_cmnd_block));
+	atapi_cmnd->pktlen = 12;
+	atapi_cmnd->blocks = 1;
+	atapi_cmnd->block_size = block_size; /* transfer size is block_size * blocks */
+	atapi_cmnd->proto = proto;
+	atapi_cmnd->in_out = type;
 }
 
 static inline int sys_storage_send_atapi_command(uint32_t fd, struct lv2_atapi_cmnd_block *atapi_cmnd, uint8_t *buffer) {
-    uint64_t tag;
-    system_call_7(SYS_STORAGE_EXECUTE_DEVICE_COMMAND
-            , fd
-            , LV2_STORAGE_SEND_ATAPI_COMMAND
-            , (uint32_t) atapi_cmnd
-            , sizeof (struct lv2_atapi_cmnd_block)
-            , (uint32_t) buffer
-            , atapi_cmnd->block_size
-            , (uint32_t) & tag);
+	uint64_t tag;
+	system_call_7(SYS_STORAGE_EXECUTE_DEVICE_COMMAND
+	              , fd
+	              , LV2_STORAGE_SEND_ATAPI_COMMAND
+	              , (uint32_t) atapi_cmnd
+	              , sizeof (struct lv2_atapi_cmnd_block)
+	              , (uint32_t) buffer
+	              , atapi_cmnd->block_size
+	              , (uint32_t) &tag);
 
-    return_to_user_prog(int);
+	return_to_user_prog(int);
 }
 
 static inline int sys_storage_async_configure(uint32_t fd, int io_buffer, sys_event_queue_t equeue_id, int *unknown) {
-    system_call_4(SYS_STORAGE_ASYNC_CONFIGURE
-            , fd
-            , io_buffer
-            , equeue_id
-            , (uint32_t) unknown);
+	system_call_4(SYS_STORAGE_ASYNC_CONFIGURE
+	              , fd
+	              , io_buffer
+	              , equeue_id
+	              , (uint32_t) unknown);
 
-    return_to_user_prog(int);
+	return_to_user_prog(int);
 }
 
 static inline int sys_storage_get_device_info(uint64_t device, uint8_t *buffer) {
-    system_call_2(SYS_STORAGE_GET_DEVICE_INFO, device, (uint32_t) buffer);
-    return_to_user_prog(int);
+	system_call_2(SYS_STORAGE_GET_DEVICE_INFO, device, (uint32_t) buffer);
+	return_to_user_prog(int);
 }
 
 static inline int sys_storage_open(uint64_t id, int *fd) {
-    system_call_4(SYS_STORAGE_OPEN, id, 0, (uint32_t) fd, 0);
-    return_to_user_prog(int);
+	system_call_4(SYS_STORAGE_OPEN, id, 0, (uint32_t) fd, 0);
+	return_to_user_prog(int);
 }
 
 static inline int sys_storage_close(int fd) {
-    system_call_1(SYS_STORAGE_CLOSE, fd);
-    return_to_user_prog(int);
+	system_call_1(SYS_STORAGE_CLOSE, fd);
+	return_to_user_prog(int);
 }
 
 static inline int sys_storage_read(int fd, uint32_t start_sector, uint32_t sectors, uint8_t *bounce_buf, uint8_t *sectors_read) {
-    system_call_7(SYS_STORAGE_READ, fd, 0, start_sector, sectors, (uint32_t) bounce_buf, (uint32_t) sectors_read, 0);
-    return_to_user_prog(int);
+	system_call_7(SYS_STORAGE_READ, fd, 0, start_sector, sectors, (uint32_t) bounce_buf, (uint32_t) sectors_read, 0);
+	return_to_user_prog(int);
 }
 
 static inline int sys_storage_async_read(int fd, uint32_t start_sector, uint32_t sectors, uint8_t *unknown1, uint64_t user_data) {
-    system_call_7(SYS_STORAGE_ASYNC_READ, fd, 0, start_sector, sectors, (uint32_t) unknown1, user_data, 0);
-    return_to_user_prog(int);
+	system_call_7(SYS_STORAGE_ASYNC_READ, fd, 0, start_sector, sectors, (uint32_t) unknown1, user_data, 0);
+	return_to_user_prog(int);
 }
 
 static inline int sys_io_buffer_create(int *io_buffer, int buffer_count) {
@@ -151,12 +151,12 @@ static inline int sys_io_buffer_free(int io_buffer, int block) {
 }
 
 static inline void sys_storage_reset_bd(void) {
-  system_call_2(864, 0x5004, 0x29);
-}   
+	system_call_2(864, 0x5004, 0x29);
+}
 
 static inline void sys_storage_authenticate_bd(void) {
-  int func = 0x43;
-  system_call_2(864, 0x5007, (uint32_t) &func);
+	int func = 0x43;
+	system_call_2(864, 0x5007, (uint32_t) &func);
 }
 
 extern int ps3rom_lv2_init(int fd, uint8_t *buffer);
