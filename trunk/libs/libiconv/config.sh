0a2c35bbc7702535f5cc862af1c0e73f3459bf3c
mr_wicked@hotmail.com <mr_wicked@hotmail.com@e4565217-ec48-2de7-c894-ee36b58b2fd4>
2011-06-30 13:16:00 +0000

    partial libiconv commit (SVN SUCKS)

---
+++ trunk/libs/libiconv/config.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+## Configure the build.
+CFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
+LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib -lrt -llv2" \
+PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
+./configure --host="ppu" --enable-static --disable-shared || { exit 1; }
