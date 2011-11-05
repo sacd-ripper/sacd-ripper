cdadef7b9f831bb9ab36a0046b6000c63a867490
mr_wicked@hotmail.com <mr_wicked@hotmail.com@e4565217-ec48-2de7-c894-ee36b58b2fd4>
2011-11-05 15:35:36 +0000

    - replaced Maxims dst_decoder with a faster multi-threaded version

--- branches/0.30/libs/libdstdec/dst_decoder.h
+++ branches/0.30/libs/libdstdec/dst_decoder.h
@@ -19,62 +19,15 @@
  *
  */
 
-#ifndef __DST_DECODER_H__
-#define __DST_DECODER_H__
+#ifndef DST_DECODER_H
+#define DST_DECODER_H
 
-#include <pthread.h> 
-#include <stdint.h>
-
-#include "types.h"
-
-enum slot_state_t {SLOT_EMPTY, SLOT_LOADED, SLOT_RUNNING, SLOT_READY};
-
+typedef struct dst_decoder_s dst_decoder_t;
 typedef void (*frame_decoded_callback_t)(uint8_t* frame_data, size_t frame_size, void *userdata);
 
-typedef struct buffer_slot_t
-{
-    uint8_t*                    buf;
-    size_t                      size;
-} 
-buffer_slot_t;
+dst_decoder_t* dst_decoder_create(int channel_count, frame_decoded_callback_t frame_decoded_callback, void *userdata);
+void dst_decoder_destroy(dst_decoder_t *dst_decoder);
+void dst_decoder_decode(dst_decoder_t *dst_decoder, uint8_t* frame_data, size_t frame_size);
 
-typedef struct frame_slot_t
-{
-    int                         initialized;
-    int                         frame_nr;
-    ebunch                      D;
-    buffer_slot_t              *dsd_data_slot;
-    buffer_slot_t              *dst_data_slot;
-    pthread_t                   thread;
-    volatile int                state;
-    pthread_mutex_t             get_mutex;
-    pthread_mutex_t             put_mutex;
-} 
-frame_slot_t;
 
-typedef struct dst_decoder_t
-{
-    frame_slot_t               *frame_slots;
-
-    int                         frame_slot_count;
-    int                         current_frame_slot;
-
-    buffer_slot_t              *dsd_data_slots;
-    buffer_slot_t              *dst_data_slots;
-
-    int                         buffer_slot_count;
-    int                         current_buffer_slot;
-
-    int                         channel_count;
-    uint32_t                    frame_nr;
-
-    frame_decoded_callback_t    frame_decoded_callback;
-    void                        *userdata;
-} 
-dst_decoder_t;
-
-dst_decoder_t *dst_decoder_create(int channel_count, frame_decoded_callback_t frame_decoded_callback, void *userdata);
-int dst_decoder_destroy(dst_decoder_t *dst_decoder);
-int dst_decoder_decode(dst_decoder_t *dst_decoder, uint8_t *dst_data, size_t dst_size);
-
-#endif // __DST_DECODER_H__
+#endif /* DST_DECODER_H */
