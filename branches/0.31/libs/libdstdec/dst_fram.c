2b9dd72c9b5029e00f26d5d8e1030962caaba1e7
Maxim.Anisiutkin@gmail.com <Maxim.Anisiutkin@gmail.com@e4565217-ec48-2de7-c894-ee36b58b2fd4>
2011-11-12 13:23:37 +0000

    Unsigned FIR coefficients

--- branches/0.31/libs/libdstdec/dst_fram.c
+++ branches/0.31/libs/libdstdec/dst_fram.c
@@ -62,21 +62,110 @@ Changes:
 /*       INCLUDES                                                             */
 /*============================================================================*/
 
-/* Without this, SSE2 intrinsics will be compiled in.
- * Even something as simple as detecting SSE2 capability within DST_InitDecoder()
- * then using that to determine which code path to use, impacts peformance significantly */
-//#define NO_SSE2
-
 #include <malloc.h>
 #include <memory.h>
 #include <stdio.h>
-#if !defined(NO_SSE2) && (defined(_M_IX86) || defined(_M_X64) || defined(__i386__) || defined(__x86_64__))
-#include <emmintrin.h>
-#endif
 #include "dst_ac.h"
 #include "types.h"
 #include "dst_fram.h"
 #include "unpack_dst.h"
+
+/*============================================================================*/
+/*       CONSTANTS                                                            */
+/*============================================================================*/
+
+#define PBITS   AC_BITS             /* number of bits for Probabilities             */
+#define NBITS   4                   /* number of overhead bits: must be at least 2! */
+                                    /* maximum "variable shift length" is (NBITS-1) */
+#define PSUM    (1 << (PBITS))
+#define ABITS   (PBITS + NBITS)     /* must be at least PBITS+2     */
+#define MB      0                   /* if (MB) print max buffer use */
+#define ONE     (1 << ABITS)
+#define HALF    (1 << (ABITS - 1))
+
+static __inline void LT_ACDecodeBit_Init(ACData *AC, uint8_t *cb, int fs)
+{
+    AC->Init = 0;
+    AC->A    = ONE - 1;
+    AC->C    = 0;
+    for (AC->cbptr = 1; AC->cbptr <= ABITS; AC->cbptr++)
+    {
+        AC->C <<= 1;
+        if (AC->cbptr < fs)
+        {
+            AC->C |= cb[AC->cbptr];
+        }
+    }
+}
+  
+static __inline void LT_ACDecodeBit_Decode(ACData *AC, uint8_t *b, int p, uint8_t *cb, int fs)
+{
+    unsigned int ap;
+    unsigned int h;
+
+    /* approximate (A * p) with "partial rounding". */
+    ap = ((AC->A >> PBITS) | ((AC->A >> (PBITS - 1)) & 1)) * p;
+    
+    h = AC->A - ap;
+    if (AC->C >= h)
+    {
+        *b = 0;
+        AC->C -= h;
+        AC->A  = ap;
+    }
+    else
+    {
+        *b = 1;
+        AC->A  = h;
+    }
+    while (AC->A < HALF)
+    {
+        AC->A <<= 1;
+      
+        /* Use new flushing technique; insert zero in LSB of C if reading past
+            the end of the arithmetic code */
+        AC->C <<= 1;
+        if (AC->cbptr < fs)
+        {
+            AC->C |= cb[AC->cbptr];
+        }
+        AC->cbptr++;
+    }
+}
+
+static __inline void LT_ACDecodeBit_Flush(ACData *AC, uint8_t *b, int p, uint8_t *cb, int fs)
+{
+    AC->Init = 1;
+    if (AC->cbptr < fs - 7)
+    {
+        *b = 0;
+    }
+    else
+    {
+        *b = 1;
+        while ((AC->cbptr < fs) && (*b == 1))
+        {
+            if (cb[AC->cbptr] != 0)
+            {
+                *b = 1;
+            }
+            AC->cbptr++;
+        }
+    }
+}
+
+static __inline int LT_ACGetPtableIndex(int16_t PredicVal, int PtableLen)
+{
+    int  j;
+  
+    j = (PredicVal > 0 ? PredicVal : -PredicVal) >> AC_QSTEP;
+    if (j >= PtableLen)
+    {
+        j = PtableLen - 1;
+    }
+  
+    return j;
+}
 
 /***************************************************************************/
 /*                                                                         */
@@ -147,9 +236,9 @@ static int16_t Reverse7LSBs(int16_t c)
     return reverse[(c + (1 << SIZE_PREDCOEF)) & 127];
 }
 
-static void LT_PRECOMPUTE(ebunch *D, int16_t LT_ICoefA[2 * MAX_CHANNELS][16][256], uint8_t LT_Status[MAX_CHANNELS][16])
+static void LT_InitCoefTablesI(ebunch *D, int16_t ICoefI[2 * MAX_CHANNELS][16][256])
 {
-    int FilterNr, FilterLength, TableNr, k, i, j, ChNr;
+    int FilterNr, FilterLength, TableNr, k, i, j;
 
     for (FilterNr = 0; FilterNr < D->FrameHdr.NrOfFilters; FilterNr++)
     {
@@ -167,23 +256,99 @@ static void LT_PRECOMPUTE(ebunch *D, int16_t LT_ICoefA[2 * MAX_CHANNELS][16][256
             }
             for (i = 0; i < 256; i++)
             {
-                int16_t cvalue = 0;
+                int cvalue = 0;
+                for (j = 0; j < k; j++)
+                {
+                    cvalue += (((i >> j) & 1) * 2 - 1) * D->FrameHdr.ICoefA[FilterNr][TableNr * 8 + j];
+                }
+                ICoefI[FilterNr][TableNr][i] = (int16_t)cvalue;
+            }
+        }
+    }
+}
+
+static void LT_InitCoefTablesU(ebunch *D, uint16_t ICoefU[2 * MAX_CHANNELS][16][256])
+{
+    int FilterNr, FilterLength, TableNr, k, i, j;
+
+    for (FilterNr = 0; FilterNr < D->FrameHdr.NrOfFilters; FilterNr++)
+    {
+        FilterLength = D->FrameHdr.PredOrder[FilterNr];
+        for (TableNr = 0; TableNr < 16; TableNr++)
+        {
+            k = FilterLength - TableNr * 8;
+            if (k > 8)
+            {
+                k = 8;
+			}
+			else if (k < 0)
+			{
+                k = 0;
+            }
+            for (i = 0; i < 256; i++)
+            {
+                int cvalue = 0;
                 for (j = 0; j < k; j++)
                 {
                     cvalue += (int16_t)(((i >> j) & 1) * 2 - 1) * D->FrameHdr.ICoefA[FilterNr][TableNr * 8 + j];
                 }
-                LT_ICoefA[FilterNr][TableNr][i] = cvalue;
+                ICoefU[FilterNr][TableNr][i] = (uint16_t)(cvalue + (1 << SIZE_PREDCOEF) * 8);
             }
         }
     }
+}
+
+static void LT_InitStatus(ebunch *D, uint8_t Status[MAX_CHANNELS][16])
+{
+    int ChNr, TableNr;
 
     for (ChNr = 0; ChNr < D->FrameHdr.NrOfChannels; ChNr++)
     {
         for (TableNr = 0; TableNr < 16; TableNr++)
         {
-            LT_Status[ChNr][TableNr] = 0xaa;
+            Status[ChNr][TableNr] = 0xaa;
         }
     }
+}
+
+static int16_t LT_RunFilterI(int16_t FilterTable[16][256], uint8_t ChannelStatus[16])
+{
+    int Predict;
+
+    Predict  = FilterTable[ 0][ChannelStatus[ 0]];
+    Predict += FilterTable[ 1][ChannelStatus[ 1]];
+    Predict += FilterTable[ 2][ChannelStatus[ 2]];
+    Predict += FilterTable[ 3][ChannelStatus[ 3]];
+    Predict += FilterTable[ 4][ChannelStatus[ 4]];
+    Predict += FilterTable[ 5][ChannelStatus[ 5]];
+    Predict += FilterTable[ 6][ChannelStatus[ 6]];
+    Predict += FilterTable[ 7][ChannelStatus[ 7]];
+    Predict += FilterTable[ 8][ChannelStatus[ 8]];
+    Predict += FilterTable[ 9][ChannelStatus[ 9]];
+    Predict += FilterTable[10][ChannelStatus[10]];
+    Predict += FilterTable[11][ChannelStatus[11]];
+    Predict += FilterTable[12][ChannelStatus[12]];
+    Predict += FilterTable[13][ChannelStatus[13]];
+    Predict += FilterTable[14][ChannelStatus[14]];
+    Predict += FilterTable[15][ChannelStatus[15]];
+    return (int16_t)Predict;
+}
+
+static int16_t LT_RunFilterU(uint16_t FilterTable[16][256], uint8_t ChannelStatus[16])
+{
+    uint32_t Predict32;
+    int      Predict;
+
+    Predict32  = FilterTable[ 0][ChannelStatus[ 0]] | (FilterTable[ 1][ChannelStatus[ 1]] << 16);
+    Predict32 += FilterTable[ 2][ChannelStatus[ 2]] | (FilterTable[ 3][ChannelStatus[ 3]] << 16);
+    Predict32 += FilterTable[ 4][ChannelStatus[ 4]] | (FilterTable[ 5][ChannelStatus[ 5]] << 16);
+    Predict32 += FilterTable[ 6][ChannelStatus[ 6]] | (FilterTable[ 7][ChannelStatus[ 7]] << 16);
+    Predict32 += FilterTable[ 8][ChannelStatus[ 8]] | (FilterTable[ 9][ChannelStatus[ 9]] << 16);
+    Predict32 += FilterTable[10][ChannelStatus[10]] | (FilterTable[11][ChannelStatus[11]] << 16);
+    Predict32 += FilterTable[12][ChannelStatus[12]] | (FilterTable[13][ChannelStatus[13]] << 16);
+    Predict32 += FilterTable[14][ChannelStatus[14]] | (FilterTable[15][ChannelStatus[15]] << 16);
+    Predict = (Predict32 >> 16) + (Predict32 & 0xffff);
+    return (int16_t)Predict;
 }
 
 /***************************************************************************/
@@ -200,11 +365,7 @@ static void LT_PRECOMPUTE(ebunch *D, int16_t LT_ICoefA[2 * MAX_CHANNELS][16][256
 /* post     : D->WM.Pwm                                                    */
 /*                                                                         */
 /***************************************************************************/
-
-#define FilterTable LT_ICoefA[Filter]
-#define ChannelStatus LT_Status[ChNr]
-
-#define LT_CHANNEL_FIR() \
+#define LT_RUN_FILTER_I(FilterTable, ChannelStatus) \
     Predict  = FilterTable[ 0][ChannelStatus[ 0]]; \
     Predict += FilterTable[ 1][ChannelStatus[ 1]]; \
     Predict += FilterTable[ 2][ChannelStatus[ 2]]; \
@@ -222,91 +383,25 @@ static void LT_PRECOMPUTE(ebunch *D, int16_t LT_ICoefA[2 * MAX_CHANNELS][16][256
     Predict += FilterTable[14][ChannelStatus[14]]; \
     Predict += FilterTable[15][ChannelStatus[15]];
 
-#if !defined(NO_SSE2) && (defined(_M_IX86) || defined(_M_X64) || defined(__i386__) || defined(__x86_64__))
-#define LT_CHANNEL_FIR_SSE2() \
-{ \
-    __m128i p0, p1, s; \
-    \
-    p0 = _mm_set_epi16( \
-        FilterTable[ 7][ChannelStatus[ 7]], \
-        FilterTable[ 6][ChannelStatus[ 6]], \
-        FilterTable[ 5][ChannelStatus[ 5]], \
-        FilterTable[ 4][ChannelStatus[ 4]], \
-        FilterTable[ 3][ChannelStatus[ 3]], \
-        FilterTable[ 2][ChannelStatus[ 2]], \
-        FilterTable[ 1][ChannelStatus[ 1]], \
-        FilterTable[ 0][ChannelStatus[ 0]] \
-    ); \
-    p1 = _mm_set_epi16( \
-        FilterTable[15][ChannelStatus[15]], \
-        FilterTable[14][ChannelStatus[14]], \
-        FilterTable[13][ChannelStatus[13]], \
-        FilterTable[12][ChannelStatus[12]], \
-        FilterTable[11][ChannelStatus[11]], \
-        FilterTable[10][ChannelStatus[10]], \
-        FilterTable[ 9][ChannelStatus[ 9]], \
-        FilterTable[ 8][ChannelStatus[ 8]] \
-    ); \
-    s = _mm_add_epi16(p0, p1); \
-    s = _mm_add_epi16(s, _mm_shuffle_epi32(s, _MM_SHUFFLE(2, 3, 0, 1))); \
-    s = _mm_add_epi16(s, _mm_shuffle_epi32(s, _MM_SHUFFLE(0, 1, 2, 3))); \
-    s = _mm_add_epi16(s, _mm_srli_si128(s, 2)); \
-    \
-    /* SSSE3 specific instructions, that aren't any faster on Nehalem */ \
-    /* s = _mm_hadds_epi16(s, s); */ \
-    /* s = _mm_hadds_epi16(s, s); */ \
-    /* s = _mm_hadds_epi16(s, s); */ \
-    \
-    Predict = (int16_t)(_mm_cvtsi128_si32(s) & 0xffff); \
-}
-#endif
-
-#define LT_CHANNEL_BIT(method, bit, oper) \
-    for (ChNr = 0; ChNr < NrOfChannels; ChNr++) \
+#define LT_RUN_FILTER_U(FilterTable, ChannelStatus) \
     { \
-        /* Calculate output value of the FIR filter */ \
-        int16_t Predict; \
-        uint8_t Residual; \
-        int16_t BitVal; \
-        \
-        const int Filter = D->FrameHdr.Filter4Bit[ChNr][BitNr]; \
-        \
-        method; \
-        \
-        /* Arithmetic decode the incoming bit */ \
-        if ((D->FrameHdr.HalfProb[ChNr]/* == 1*/) && (BitNr < D->FrameHdr.NrOfHalfBits[ChNr])) \
-        { \
-            DST_ACDecodeBit(&AC, &Residual, AC_PROBS / 2, D->AData, D->ADataLen, 0); \
-        } \
-        else \
-        { \
-            const int table4bit = D->FrameHdr.Ptable4Bit[ChNr][BitNr]; \
-            const int PtableIndex = DST_ACGetPtableIndex(Predict, D->FrameHdr.PtableLen[table4bit]); \
-            \
-            DST_ACDecodeBit(&AC, &Residual, D->P_one[table4bit][PtableIndex], D->AData, D->ADataLen, 0); \
-        } \
-        \
-        /* Channel bit depends on the predicted bit and BitResidual[][] */ \
-        BitVal = ((((uint16_t)Predict) >> 15) ^ Residual) & 1; \
-        \
-        /* Shift the result into the correct bit position */ \
-        MuxedDSD[ChNr] oper (uint8_t)(BitVal << bit); \
-        \
-        /* Update filter */ \
-        { \
-            uint32_t* st = (uint32_t*)LT_Status[ChNr]; \
-            st[3] = (st[3] << 1) | ((st[2] >> 31) & 1); \
-            st[2] = (st[2] << 1) | ((st[1] >> 31) & 1); \
-            st[1] = (st[1] << 1) | ((st[0] >> 31) & 1); \
-            st[0] = (st[0] << 1) | BitVal; \
-        } \
-    } \
-    BitNr++;
+        uint32_t Predict32; \
+         \
+        Predict32  = FilterTable[ 0][ChannelStatus[ 0]] | (FilterTable[ 1][ChannelStatus[ 1]] << 16); \
+        Predict32 += FilterTable[ 2][ChannelStatus[ 2]] | (FilterTable[ 3][ChannelStatus[ 3]] << 16); \
+        Predict32 += FilterTable[ 4][ChannelStatus[ 4]] | (FilterTable[ 5][ChannelStatus[ 5]] << 16); \
+        Predict32 += FilterTable[ 6][ChannelStatus[ 6]] | (FilterTable[ 7][ChannelStatus[ 7]] << 16); \
+        Predict32 += FilterTable[ 8][ChannelStatus[ 8]] | (FilterTable[ 9][ChannelStatus[ 9]] << 16); \
+        Predict32 += FilterTable[10][ChannelStatus[10]] | (FilterTable[11][ChannelStatus[11]] << 16); \
+        Predict32 += FilterTable[12][ChannelStatus[12]] | (FilterTable[13][ChannelStatus[13]] << 16); \
+        Predict32 += FilterTable[14][ChannelStatus[14]] | (FilterTable[15][ChannelStatus[15]] << 16); \
+        Predict = (Predict32 >> 16) + (Predict32 & 0xffff); \
+    }
 
 int DST_FramDSTDecode(uint8_t *DSTdata, uint8_t *MuxedDSDdata, int FrameSizeInBytes, int FrameCnt, ebunch *D)
 {
     int       retval = 0;
-    int       BitNr, BitIndexNr;
+    int       BitNr;
     int       ChNr;
     uint8_t   ACError;
     const int NrOfBitsPerCh = D->FrameHdr.NrOfBitsPerCh;
@@ -323,53 +418,70 @@ int DST_FramDSTDecode(uint8_t *DSTdata, uint8_t *MuxedDSDdata, int FrameSizeInBy
     if (D->FrameHdr.DSTCoded == 1)
     {
         ACData AC;
-        __declspec(align(16)) int16_t LT_ICoefA[2 * MAX_CHANNELS][16][256];
-        __declspec(align(16)) uint8_t LT_Status[MAX_CHANNELS][16];
+        __declspec(align(16)) int16_t  LT_ICoefI[2 * MAX_CHANNELS][16][256];
+        //__declspec(align(16)) uint16_t LT_ICoefU[2 * MAX_CHANNELS][16][256];
+        __declspec(align(16)) uint8_t  LT_Status[MAX_CHANNELS][16];
 
         FillTable4Bit(NrOfChannels, NrOfBitsPerCh, &D->FrameHdr.FSeg, D->FrameHdr.Filter4Bit);
         FillTable4Bit(NrOfChannels, NrOfBitsPerCh, &D->FrameHdr.PSeg, D->FrameHdr.Ptable4Bit);
 
-        LT_PRECOMPUTE(D, LT_ICoefA, LT_Status);
+        LT_InitCoefTablesI(D, LT_ICoefI);
+        //LT_InitCoefTablesU(D, LT_ICoefU);
+        LT_InitStatus(D, LT_Status);
 
-        AC.Init = 1;
-        DST_ACDecodeBit(&AC, &ACError, Reverse7LSBs(D->FrameHdr.ICoefA[0][0]), D->AData, D->ADataLen, 0);
+        LT_ACDecodeBit_Init(&AC, D->AData, D->ADataLen);
+        LT_ACDecodeBit_Decode(&AC, &ACError, Reverse7LSBs(D->FrameHdr.ICoefA[0][0]), D->AData, D->ADataLen);
 
-        BitIndexNr = 0;
-
-        /* icc predicts the 'else' case to have higher probabiilty */
-        if (!D->SSE2 || 1)
+        memset(MuxedDSD, 0, NrOfBitsPerCh * NrOfChannels / 8); 
+        for (BitNr = 0; BitNr < NrOfBitsPerCh; BitNr++)
         {
-            for (BitNr = 0; BitNr < NrOfBitsPerCh - 7; MuxedDSD += NrOfChannels)
+            int ByteNr = BitNr / 8;
+
+            for (ChNr = 0; ChNr < NrOfChannels; ChNr++)
             {
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR(), 7,  =);
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR(), 6, |=);
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR(), 5, |=);
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR(), 4, |=);
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR(), 3, |=);
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR(), 2, |=);
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR(), 1, |=);
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR(), 0, |=);
+                int16_t Predict;
+                uint8_t Residual;
+                int16_t BitVal;
+                const int Filter = D->FrameHdr.Filter4Bit[ChNr][BitNr];
+
+                /* Calculate output value of the FIR filter */
+                LT_RUN_FILTER_I(LT_ICoefI[Filter], LT_Status[ChNr]);
+                //LT_RUN_FILTER_U(LT_ICoefU[Filter], LT_Status[ChNr]);
+                //Predict = LT_RunFilterI(LT_ICoefI[Filter], LT_Status[ChNr]);
+                //Predict = LT_RunFilterU(LT_ICoefU[Filter], LT_Status[ChNr]);
+
+                /* Arithmetic decode the incoming bit */
+                if ((D->FrameHdr.HalfProb[ChNr]/* == 1*/) && (BitNr < D->FrameHdr.NrOfHalfBits[ChNr]))
+                {
+                    LT_ACDecodeBit_Decode(&AC, &Residual, AC_PROBS / 2, D->AData, D->ADataLen);
+                }
+                else
+                {
+                    const int table4bit = D->FrameHdr.Ptable4Bit[ChNr][BitNr];
+                    const int PtableIndex = LT_ACGetPtableIndex(Predict, D->FrameHdr.PtableLen[table4bit]);
+
+                    LT_ACDecodeBit_Decode(&AC, &Residual, D->P_one[table4bit][PtableIndex], D->AData, D->ADataLen);
+                }
+
+                /* Channel bit depends on the predicted bit and BitResidual[][] */
+                BitVal = ((((uint16_t)Predict) >> 15) ^ Residual) & 1;
+
+                /* Shift the result into the correct bit position */ \
+                MuxedDSD[ByteNr * NrOfChannels + ChNr] |= (uint8_t)(BitVal << (7 - BitNr % 8));
+
+                /* Update filter */
+                {
+                    uint32_t* const st = (uint32_t*)LT_Status[ChNr];
+                    st[3] = (st[3] << 1) | ((st[2] >> 31) & 1);
+                    st[2] = (st[2] << 1) | ((st[1] >> 31) & 1);
+                    st[1] = (st[1] << 1) | ((st[0] >> 31) & 1);
+                    st[0] = (st[0] << 1) | BitVal;
+                }
             }
         }
-#if !defined(NO_SSE2) && (defined(_M_IX86) || defined(_M_X64) || defined(__i386__) || defined(__x86_64__))
-        else
-        {
-            for (BitNr = 0; BitNr < NrOfBitsPerCh - 7; MuxedDSD += NrOfChannels)
-            {
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR_SSE2(), 7,  =);
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR_SSE2(), 6, |=);
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR_SSE2(), 5, |=);
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR_SSE2(), 4, |=);
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR_SSE2(), 3, |=);
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR_SSE2(), 2, |=);
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR_SSE2(), 1, |=);
-                LT_CHANNEL_BIT(LT_CHANNEL_FIR_SSE2(), 0, |=);
-            }
-        }
-#endif
 
         /* Flush the arithmetic decoder */
-        DST_ACDecodeBit(&AC, &ACError, 0, D->AData, D->ADataLen, 1);
+        LT_ACDecodeBit_Flush(&AC, &ACError, 0, D->AData, D->ADataLen);
 
         if (ACError != 1)
         {
