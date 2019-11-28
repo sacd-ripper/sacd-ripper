1e916f47021633af52fe6cc383e486dab5382d0a
EuFlo <freefev@gmail.com>
2019-11-28 12:34:40 +0200

    Update sacd_input.h

--- libs/libsacd/sacd_input.h
+++ libs/libsacd/sacd_input.h
@@ -19,41 +19,53 @@
  *
  */
 
+#define _FILE_OFFSET_BITS 64    /* large file support - linux */
 #ifndef SACD_INPUT_H_INCLUDED
 #define SACD_INPUT_H_INCLUDED
 
 #include <inttypes.h>
 
-#if defined(__MINGW32__) || defined(_WIN32)
-#   undef  lseek
-#   define lseek            _lseeki64
-#   undef  fseeko
-#   define fseeko           fseeko64
-#   undef  ftello
-#   define ftello           ftello64
-#   define flockfile(...)
-#   define funlockfile(...)
-#   define getc_unlocked    getc
-#   undef  off_t
-#   define off_t            uint64_t
-#   undef  stat
-#   define stat             _stati64
-#   define fstat            _fstati64
-#   define wstat            _wstati64
-#endif
+
+//  #if defined(__MINGW32__) || defined(_WIN32)
+//  #   undef  lseek
+//  #   define lseek            _lseeki64
+//  #   undef  fseeko
+//  #   define fseeko           fseeko64
+//  #   undef  ftello
+//  #   define ftello           ftello64
+//  #   define flockfile(...)
+//  #   define funlockfile(...)
+//  #   define getc_unlocked    getc
+//  #   undef  off_t
+//  #   define off_t            __int64
+//  //#   define off_t            uint64_t
+//  #   undef  stat
+//  #   define stat             _stati64
+//  #   define fstat            _fstati64
+//  #   define wstat            _wstati64
+//  #endif
+
+
+//64 bit file access is the natural file access type for Cygwin. off_t is 8 bytes. 
+//There are no foo64 functions for that reason. 
+//Just use fopen and friends and you get 64 bit file access for free.
 
 #if defined(__lv2ppu__)
 #include <sys/io_buffer.h>
 #endif
 
+// for linux it is already added preprocessor coomand in cmake text 
+//   -D_FILE_OFFSET_BITS=64
+// #define _FILE_OFFSET_BITS 64
+
 typedef struct sacd_input_s * sacd_input_t;
 
 extern sacd_input_t (*sacd_input_open)         (const char *);
 extern int          (*sacd_input_close)        (sacd_input_t);
-extern ssize_t      (*sacd_input_read)         (sacd_input_t, int, int, void *);
+extern uint32_t     (*sacd_input_read)         (sacd_input_t, uint32_t, uint32_t, void *);
 extern char *       (*sacd_input_error)        (sacd_input_t);
 extern int          (*sacd_input_authenticate) (sacd_input_t);
-extern int          (*sacd_input_decrypt)      (sacd_input_t, uint8_t *, int);
+extern int          (*sacd_input_decrypt)      (sacd_input_t, uint8_t *, uint32_t);
 extern uint32_t     (*sacd_input_total_sectors)(sacd_input_t);
 
 int sacd_input_setup(const char *); 
