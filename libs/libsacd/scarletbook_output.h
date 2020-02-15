2ed91ffb05e2024c9293f6f69b25e7fd46e20cdc
EuFlo <freefev@gmail.com>
2020-02-15 23:18:17 +0200

    Added nopad option

--- libs/libsacd/scarletbook_output.h
+++ libs/libsacd/scarletbook_output.h
@@ -86,6 +86,7 @@ struct scarletbook_output_format_t
 
     scarletbook_handle_t           *sb_handle;
     fwprintf_callback_t             cb_fwprintf;
+    int                             dsf_nopad;
 
     struct list_head                siblings;
 }; 
@@ -97,7 +98,7 @@ typedef void (*stats_track_callback_t)(char *filename, int current_track, int to
 
 scarletbook_output_t *scarletbook_output_create(scarletbook_handle_t *, stats_track_callback_t, stats_progress_callback_t, fwprintf_callback_t);
 int scarletbook_output_destroy(scarletbook_output_t *);
-int scarletbook_output_enqueue_track(scarletbook_output_t *, int, int, char *, char *, int);
+int scarletbook_output_enqueue_track(scarletbook_output_t *, int, int, char *, char *, int, int);
 int scarletbook_output_enqueue_raw_sectors(scarletbook_output_t *, int, int, char *, char *);
 int scarletbook_output_start(scarletbook_output_t *);
 void scarletbook_output_interrupt(scarletbook_output_t *);
